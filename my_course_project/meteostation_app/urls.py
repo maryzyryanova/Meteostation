9d6f7e96c2a2a3bf64019240f7e26571ed049376
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-12-10 14:03:40 +0300

    url added

--- my_course_project/meteostation_app/urls.py
+++ my_course_project/meteostation_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MetricsView
+from .views import MetricsView, WeatherForecastView
 
 urlpatterns = [
     path('send/', MetricsView.as_view()),
+    path('', WeatherForecastView.as_view()),
 ]
