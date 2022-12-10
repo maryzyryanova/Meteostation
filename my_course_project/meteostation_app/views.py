bb9f9410339f11c517c90390f5e08c2c233f2e91
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-12-10 14:03:49 +0300

    initial commit

--- my_course_project/meteostation_app/views.py
+++ my_course_project/meteostation_app/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from .serializer import MetricsSerializer
+from django.views import View
+from .models import Metrics
+from django.shortcuts import render
 
 class MetricsView(generics.GenericAPIView):
     serializer_class = MetricsSerializer
@@ -11,3 +14,18 @@ class MetricsView(generics.GenericAPIView):
             serializer.save()
             return Response(data = "OK", status = 201)
         return Response(data = "ERROR", status = 400)
+
+class WeatherForecastView(View):
+    template_name = "weather_forecast.html"
+
+    def get(self, request):
+        metrics = Metrics.objects.all().order_by("-datetime")[:0]
+        return render(
+            request,
+            self.template_name,
+            {
+                "metrics": metrics,
+            },
+        )
+
+ 
--- my_course_project/modules/data/data.ino
+++ my_course_project/modules/data/data.ino
@@ -1,22 +1,32 @@
 #include <ESP8266WiFi.h> 
 #include <ESP8266HTTPClient.h> 
+#include <Adafruit_BMP280.h>
 #include "DHT.h"
+#include <Wire.h> 
 
 #define DHTPIN D4
+#define BMP280_I2C_ADDRESS  0x76
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11 
+#define BMP_CS 10
 
 DHT dht(DHTPIN, DHT11);
+Adafruit_BMP280 bmp280;
 
 const char* ssid = "pipipupu"; 
 const char* password = "12345876"; 
 const char* host = "http://192.168.0.100"; 
 String url = "/send/"; 
 
-float temperature, humidity;
+float temperature, humidity, pressure;
 
 void setup() 
 { 
   Serial.begin(9600); 
   dht.begin();
+  bmp280.begin(BMP280_I2C_ADDRESS);
 
   delay(10); 
   Serial.println(); 
@@ -43,6 +53,7 @@ void loop()
   delay(5000); 
   temperature = dht.readTemperature(); 
   humidity = dht.readHumidity();
+  pressure = bmp280.readPressure();
 
   Serial.print("connecting to "); 
   Serial.println(host); 
@@ -52,7 +63,7 @@ void loop()
 
   Serial.print("Requesting URL: "); 
   Serial.println(url); 
-  String postData = "Temperature: " + String(temperature) + "C\n" + "Humidity: " + String(humidity) + "%\n";
+  String postData = "Temperature: " + String(temperature) + "C\n" + "Humidity: " + String(humidity) + "%\n" + "Pressure: " + String(pressure) + " Pa\n";
   Serial.println(postData); 
   String address = host + url; 
 
@@ -69,4 +80,5 @@ void loop()
   Serial.println(payload); 
   http.end(); 
   Serial.println("closing connection"); 
-} 
+}
+ 
