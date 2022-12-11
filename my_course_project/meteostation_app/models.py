412819fddfc472fa1d07d9e36375dffc4ff6097a
DenVilk <vvelikovich@gmail.com>
2022-12-12 02:53:11 +0300

    fixed bugs

--- my_course_project/meteostation_app/models.py
+++ my_course_project/meteostation_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Metrics(models.Model):
-    datetime = models.DateTimeField(auto_now = True, blank = False)
+    datetime = models.DateTimeField(auto_now_add = True, blank = False)
     temperature = models.FloatField()
     humidity = models.FloatField()
     pressure = models.FloatField()
